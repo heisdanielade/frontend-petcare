340f18f7935cd4e8da80ccfe17d84fd1f4bf3a87
heisdanielade <heisdanielade@gmail.com>
2025-07-31 13:37:58 +0200

    update: api endpoint

--- src/services/user.js
+++ src/services/user.js
@@ -1,19 +1,15 @@
 import api from "./api";
-import { useUserStore } from '@/stores/tempUser';
-
-
+import { useUserStore } from "@/stores/tempUser";
 
 export default {
   // Get user details
   async userDetails() {
     try {
-        const token = localStorage.getItem("token");
-        const response = await api.get("/user", token);
-        return response.data;
+      const token = localStorage.getItem("token");
+      const response = await api.get("/user/me", token);
+      return response.data;
     } catch (error) {
-        throw error;
+      throw error;
     }
   },
-
-
 };
