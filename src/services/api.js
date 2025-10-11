76c42fece72a3f8ee3f46d35db9df5c69a1931a5
heisdanielade <heisdanielade@gmail.com>
2025-10-11 23:51:42 +0200

    chore: env modifications

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/v1";
-// const API_URL = "https://api-petcare.com/v1";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
